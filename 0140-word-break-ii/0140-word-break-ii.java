d08a50f1621f481303fe0a5220566d93de11d596
Shaily Singh Parihar <97668606+jhallan24@users.noreply.github.com>
2024-05-26 00:07:08 +0530

    Time: 5 ms (33.46%), Space: 41.7 MB (54.99%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+        public List<String> wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        Map<String, List<String>> memo = new HashMap<>();
+        return wordBreakHelper(s, wordSet, memo);
+    }
+
+    
+    private List<String> wordBreakHelper(String s, Set<String> wordSet, Map<String, List<String>> memo) {
+        
+        if (memo.containsKey(s)) {
+            return memo.get(s);
+        }
+
+        List<String> result = new ArrayList<>();
+        
+        
+        if (wordSet.contains(s)) {
+            result.add(s);
+        }
+
+        
+        for (int i = 1; i < s.length(); i++) {
+            String prefix = s.substring(0, i);
+            if (wordSet.contains(prefix)) {
+                String suffix = s.substring(i);
+                List<String> suffixBreaks = wordBreakHelper(suffix, wordSet, memo);
+                for (String segment : suffixBreaks) {
+                    result.add(prefix + " " + segment);
+                }
+            }
+        }
+
+        
+        memo.put(s, result);
+        return result;
+    }
+
+    
+    }
+
+

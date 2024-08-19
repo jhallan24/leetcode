7905b31bea8cf3e1168e0784dd779bda2bbb68c1
Shaily Singh Parihar <97668606+jhallan24@users.noreply.github.com>
2024-08-19 15:59:21 +0530

    Time: 0 ms (100.00%), Space: 40 MB (94.15%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -0,0 +1,16 @@
+class Solution {
+    
+    public int minSteps(int n) {
+        if (n == 1) return 0; 
+
+        int steps = 0;
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                steps += i;
+                n /= i;
+            }
+        }
+        return steps;
+   
+    }
+}

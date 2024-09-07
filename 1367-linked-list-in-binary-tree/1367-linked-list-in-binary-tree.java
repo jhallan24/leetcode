95409455b40826cb3e183f089d0a773a8a6322cd
Shaily Singh Parihar <97668606+jhallan24@users.noreply.github.com>
2024-09-08 00:15:43 +0530

    Time: 1 ms (97.88%), Space: 44.9 MB (55.38%) - LeetHub

---
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+        if (checkPath(head, root)) {
+            return true;
+        }
+        
+        return isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+    
+   private boolean checkPath(ListNode head, TreeNode node) {
+        if (head == null) {
+           
+            return true;
+        }
+        if (node == null) {
+            
+            return false;
+        }
+        if (head.val != node.val) {
+           
+            return false;
+        }
+        
+        return checkPath(head.next, node.left) || checkPath(head.next, node.right);
+    }
+}
